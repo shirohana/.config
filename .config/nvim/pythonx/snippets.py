447a0386004624138a4ef910cc6ccc3070a91b06
Hana Shiro <shirohana0608@gmail.com>
2018-11-01 17:00:24 +0800

    Update stylus snippets

--- .config/nvim/pythonx/snippets.py
+++ .config/nvim/pythonx/snippets.py
@@ -4,5 +4,13 @@ def expand(snip, expand_pos_list=[1]):
     if snip.tabstop in expand_pos_list:
         vim.eval('feedkeys("\<C-R>=UltiSnips#ExpandSnippet()\<CR>")')
 
-def unit(num, has_unit, default_unit='px'):
-    return (num if has_unit else num + default_unit) if num else ''
+def unit(num, unit, default_unit='px'):
+    if num is None:
+        return ''
+    elif unit is '' or unit is None:
+        return num + default_unit
+    else:
+        return num + {
+            'e': 'em',
+            'r': 'rem'
+        }.get(unit, unit)
