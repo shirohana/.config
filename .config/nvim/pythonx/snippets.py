e316fd0b749de66fc7489048ec5446c18550b4be
Hana Shiro <shirohana0608@gmail.com>
2018-11-06 13:14:12 +0800

    Complete Stylus Snippets!

--- .config/nvim/pythonx/snippets.py
+++ .config/nvim/pythonx/snippets.py
@@ -1,16 +1,18 @@
 import vim
 
 def expand(snip, expand_pos_list=[1]):
-    if snip.tabstop in expand_pos_list:
-        vim.eval('feedkeys("\<C-R>=UltiSnips#ExpandSnippet()\<CR>")')
+  if snip.tabstop in expand_pos_list:
+    vim.eval('feedkeys("\<C-R>=UltiSnips#ExpandSnippet()\<CR>")')
 
-def unit(num, unit, default_unit='px'):
-    if num is None:
-        return ''
-    elif unit is '' or unit is None:
-        return num + default_unit
-    else:
-        return num + {
-            'e': 'em',
-            'r': 'rem'
-        }.get(unit, unit)
+def unit(num, unit, default_unit = 'rem'):
+  if num is None:
+    return ''
+  if unit is '' or unit is None:
+    return '0' if num == '0' else num + default_unit
+  else:
+    return num + {
+      'e': 'em',
+      'r': 'rem',
+      'x': 'px',
+      'p': '%'
+    }.get(unit, unit)
