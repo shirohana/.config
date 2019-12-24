7d2ce0a65d68ad1b83c7f90a6e873cc8cfef30a8
Hana Shiro <shirohana0608@gmail.com>
2019-12-24 15:09:32 +0800

    (nvim) Fix 'stylus' snippets expanding logic

--- .config/nvim/pythonx/snippets.py
+++ .config/nvim/pythonx/snippets.py
@@ -7,7 +7,7 @@ def expand(snip, expand_pos_list=[1]):
 def unit(num, unit, default_unit = 'rem'):
   if num is None:
     return ''
-  if unit is '' or unit is None:
+  if unit == '' or unit is None:
     return '0' if num == '0' else num + default_unit
   else:
     return num + {
